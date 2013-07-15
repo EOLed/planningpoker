5075e2d46a348c64edc734e6d31f8527f513404d
Amos Chan <amosschan@gmail.com>
2013-07-15 16:06:25 -0400

    Attaches deck to scope of roomController instead of view.

--- app/scripts/controllers/room.js
+++ app/scripts/controllers/room.js
@@ -2,6 +2,22 @@
 
 angular.module('planningPokerApp')
   .controller('RoomCtrl', function ($scope, $http, $routeParams, socket, userService) {
+    var deck = { type: 'mountainGoat',
+                 cards: [ { display: '0', value: '0' },
+                          { display: '&frac12;', value: '.5' },
+                          { display: '1', value: '1' },
+                          { display: '2', value: '2' },
+                          { display: '3', value: '3' },
+                          { display: '5', value: '5' },
+                          { display: '8', value: '8' },
+                          { display: '13', value: '13' },
+                          { display: '20', value: '20' },
+                          { display: '40', value: '40' },
+                          { display: '100', value: '100' },
+                          { display: '?', value: '?' },
+                          { display: '<img class="coffee" src="images/coffee.png" />',
+                            value: 'coffee' } ] };
+
     var onJoin = function (data) {
       $scope.room = data.room;
     };
@@ -10,6 +26,7 @@ angular.module('planningPokerApp')
       var eventName = 'message ' + data.key;
       socket.forward(eventName, $scope);
       $scope.room = data;
+      $scope.deck = deck;
 
       $scope.$on('socket:' + eventName, function (event, data) {
         if ( data.type === 'join') {
--- test/spec/controllers/room.js
+++ test/spec/controllers/room.js
@@ -46,6 +46,27 @@ describe('Controller: RoomCtrl', function () {
       expect(scope.room).toEqual(selectedRoom);
     });
 
+    it('should attach a deck of mountain goat cards to scope', function () {
+      var deck = { type: 'mountainGoat',
+                   cards: [ { display: '0', value: '0' },
+                            { display: '&frac12;', value: '.5' },
+                            { display: '1', value: '1' },
+                            { display: '2', value: '2' },
+                            { display: '3', value: '3' },
+                            { display: '5', value: '5' },
+                            { display: '8', value: '8' },
+                            { display: '13', value: '13' },
+                            { display: '20', value: '20' },
+                            { display: '40', value: '40' },
+                            { display: '100', value: '100' },
+                            { display: '?', value: '?' },
+                            { display: '<img class="coffee" src="images/coffee.png" />',
+                              value: 'coffee' } ] };
+      RoomCtrl = createController();
+      $httpBackend.flush();
+      expect(scope.deck).toEqual(deck);
+    });
+
     it('should forward room specific socket calls to scope', function () {
       var mockSocket = sinon.mock(socket)
                             .expects('forward')
